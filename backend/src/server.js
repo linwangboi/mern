1c6e41312fd862a6d05c43988c7ac41b6ebd7f07
linwangboi <linwangboi@gmail.com>
2025-12-06 15:32:00 +0000

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
+app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar.jsx'
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -10,9 +12,18 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = axios
+        const res = await axios.get('https://opulent-space-winner-x5vq5q4xq9543vppw-5001.app.github.dev/api/notes');
+        setNotes(res.data);
+        setIsRateLimited(false);
       } catch (error) {
-        
+        console.log('error fetching notes', error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error('Failed to load notes');
+        }
+      } finally {
+        setLoading(false);
       }
     }
     fetchNotes();
