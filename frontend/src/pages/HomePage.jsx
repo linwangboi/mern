d3544e7f5c9300fb7948c04ae17ce22ca7cd1735
linwangboi <linwangboi@gmail.com>
2025-12-07 10:45:11 +0000

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar.jsx'
+import NoteCard from '../components/NoteCard.jsx'
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import axios from 'axios';
 import toast from 'react-hot-toast';
@@ -32,6 +33,17 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-secondary py-10">Loading notes...</div>}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+
+          </div>
+        )}
+      </div>
     </div>
   )
 }
