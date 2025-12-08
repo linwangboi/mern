40ecf7c4b7ba7a1a511313e87e6f8fc8e0f71fc1
linwangboi <linwangboi@gmail.com>
2025-12-08 11:16:45 +0000

    .

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router';
+import {ArrowLeftIcon} from 'lucide-react';
 const CreatePage = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [loading, setLoading] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+
+  }
   return (
-    <div>CreatePage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+
+          <Link to={'/'} className='btn btn-ghost mb-6'>
+            <ArrowLeftIcon className='size-5' />
+            Back to Notes
+          </Link>
+
+          <div className='card bg-base-100'>
+            <div className='card-body'>
+              <h2 className='card-title text-2xl mb-4'>Create New Notes</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
