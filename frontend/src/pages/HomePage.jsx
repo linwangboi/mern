01722c8cfc876d1fc9ff68aa6688afd3105bf52c
linwangboi <linwangboi@gmail.com>
2025-12-09 12:15:38 +0000

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar.jsx'
 import NoteCard from '../components/NoteCard.jsx'
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
-import axios from 'axios';
 import toast from 'react-hot-toast';
 import api from '../lib/axios.js';
+import NotesNotFound from '../components/NotesNotFound.jsx';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -33,13 +32,16 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
+
       {isRateLimited && <RateLimitedUI />}
+
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
 
           </div>
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router";
+import api from "../lib/axios.js";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchNote();
+  }, [id]);
+  const handleDelete = async () => {};
+  const handleSave = async () => {};
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+      </div>
+    )
+  }
+
   return (
-    <div>NoteDetailPage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex items-center justify-between mb-6">
+          <Link to='/' className="btn btn-ghost">
+            <ArrowLeftIcon className="h-5 w-5" />
+            Back to Notes
+          </Link>
+          <button onClick={handleDelete} className="btn btn-error btn-outline">
+            <Trash2Icon className="size-5" />
+            Delete Note
+          </button>
+        </div>
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Title</span>
+              </label>
+              <input
+                type='text'
+                placeholder="Note title"
+                className="input input-bordered"
+                value={note.title}
+                onChange={(e) => setNote({ ...note, title: e.target.value })}
+              />
+            </div>
+            <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  placeholder="Write your note here..."
+                  className="textarea textarea-bordered h-32"
+                  value={note.content}
+                  onChange={(e) => setNote({ ...note, content: e.target.value })}
+                />
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary" disabled={saving} onClick={handleSave}>
+                {saving ? 'Saving...' : 'Save Changes'}
+              </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
