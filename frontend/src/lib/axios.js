2978c73c304dee4c9ba81063a981208dab1d5d17
linwangboi <linwangboi@gmail.com>
2025-12-10 10:54:31 +0000

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
 const api = axios.create({
-    baseURL: 'https://opulent-space-winner-x5vq5q4xq9543vppw-5001.app.github.dev/api'
+    baseURL: BASE_URL
 })
 
 export default api;
